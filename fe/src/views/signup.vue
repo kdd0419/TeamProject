981387014a92c553adcf014c336b1410116912bd
Junwon <2junewon@naver.com>
2019-05-16 14:40:25 +0900

    Revert "회원가입"
    This reverts commit 135f621a9ab3da0dd67b23bd06b513cfd782a9ed.

--- fe/src/views/signup.vue
+++ fe/src/views/signup.vue
@@ -3,19 +3,19 @@
     <v-layout row wrap align-center>
       <v-flex xs12>
         <v-toolbar flat>
-         <v-toolbar-title>회원가입</v-toolbar-title>
+         <v-toolbar-title>로그인</v-toolbar-title>
        </v-toolbar>
         <v-card>
           <div class="pa-3">
             <v-text-field
-                v-model="user_id"
-                label="임시아이디 : 사용자 이름"
+                v-model="email"
+                label="이메일을 입력하세요"
 
             >
             </v-text-field>
             <v-text-field
                 v-model="password"
-                label="임시 비밀번호 : 사용자 나이"
+                label="패스워드를 입력하세요"
                 type="password"
             >
             </v-text-field>
@@ -24,7 +24,7 @@
              block
              color="primary"
              @click="login()"
-            >회원가입</v-btn>
+            >로그인</v-btn>
           </div>
         </v-card>
       </v-flex>
@@ -39,8 +39,8 @@ export default {
       email: null,
       password: null,
       allUsers: [
-        { id:1, user_id: 'genie', password:'12345'},
-        { id:2, user_id: 'test',  password:'12345'}
+        { id:1, name: 'genie', email:'genie@geniesoft.io', password:'12345'},
+        { id:2, name: 'test', email:'test@geniesoft.io', password:'12345'}
       ]
     }
   },
@@ -49,12 +49,12 @@ export default {
       // 전체 유저에서 해당 이메일로 유저를 찾는다.
       let selectUser = null
       this.allUsers.forEach(user =>{
-        if(user.user_id === this.user_id) selectUser = user
+        if(user.email === this.email) selectUser = user
       })
-      if (selectUser === null) alert('아이디가 없습니다')
+      if (selectUser === null) alert('입력하신 이메일이 없습니다')
         else{
         if (selectUser.password !== this.password){
-          alert('아이디와 비밀번호가 일치하지 않습니다.')
+          alert('이메일과 비밀번호가 일치하지 않습니다.')
         }else {
           alert('회원가입을 축하합니다')
           location.href = 'Home.vue'
