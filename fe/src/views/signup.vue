304dba686d0de46b61e15620fd6758730444a009
LeeSSunwoo <tjsdngkdl@naver.com>
2019-05-16 14:47:04 +0900

    signup.vue 수정
    디비데이터를 모두 가져와 입력한 아이디가 이미 존재할 경우 회원가입 불가

--- fe/src/views/signup.vue
+++ fe/src/views/signup.vue
@@ -3,19 +3,19 @@
     <v-layout row wrap align-center>
       <v-flex xs12>
         <v-toolbar flat>
-         <v-toolbar-title>로그인</v-toolbar-title>
+         <v-toolbar-title>회원가입</v-toolbar-title>
        </v-toolbar>
         <v-card>
           <div class="pa-3">
             <v-text-field
-                v-model="email"
-                label="이메일을 입력하세요"
+                v-model="user_id"
+                label="임시아이디 : 사용자 이름"
 
             >
             </v-text-field>
             <v-text-field
                 v-model="password"
-                label="패스워드를 입력하세요"
+                label="임시 비밀번호 : 사용자 나이"
                 type="password"
             >
             </v-text-field>
@@ -24,7 +24,7 @@
              block
              color="primary"
              @click="login()"
-            >로그인</v-btn>
+            >회원가입</v-btn>
           </div>
         </v-card>
       </v-flex>
@@ -33,35 +33,46 @@
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   data () {
     return {
       email: null,
       password: null,
-      allUsers: [
-        { id:1, name: 'genie', email:'genie@geniesoft.io', password:'12345'},
-        { id:2, name: 'test', email:'test@geniesoft.io', password:'12345'}
-      ]
+      allUsers: []
     }
+  },
+  mounted () {
+    this.getUsers()
   },
   methods: {
     login() {
       // 전체 유저에서 해당 이메일로 유저를 찾는다.
       let selectUser = null
       this.allUsers.forEach(user =>{
-        if(user.email === this.email) selectUser = user
+        if(user.user_id === this.user_id) selectUser = user
       })
-      if (selectUser === null) alert('입력하신 이메일이 없습니다')
+      if (selectUser === null) alert('아이디가 없습니다')
         else{
-        if (selectUser.password !== this.password){
-          alert('이메일과 비밀번호가 일치하지 않습니다.')
+        if (selectUser === null){
+          alert('가입된 아이디 입니다.')
         }else {
-          alert('로그인 완료')
+          alert('회원가입을 축하합니다')
           location.href = 'Home.vue'
         }
       }
       // 그 유저의 비밀번호와 입력된 비밀번호를 비교한다.
       console.log(this.email, this.password)
+    },
+    getUsers () {
+      axios.get('http://localhost:3000/api/user')
+        .then((r) => {
+          this.allUsers = r.data.users
+          console.log(r)
+        })
+        .catch((e) => {
+          console.error(e.message)
+        })
     }
   }
 }
