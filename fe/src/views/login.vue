f3cd0668242cd8b7beedb7323ddec2f3d94f105a
LeeSSunwoo <tjsdngkdl@naver.com>
2019-05-16 14:04:04 +0900

    디비에서 모든 데이터가져오기
    login.js에서 모든 데이터 가져오는 함수 추가

--- fe/src/views/login.vue
+++ fe/src/views/login.vue
@@ -33,27 +33,28 @@
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   data () {
     return {
       email: null,
       password: null,
-      allUsers: [
-        { id:1, name: 'genie', email:'genie@geniesoft.io', password:'12345'},
-        { id:2, name: 'test', email:'test@geniesoft.io', password:'12345'}
-      ]
+      allUsers: []
     }
+  },
+  mounted () {
+    this.getUsers()
   },
   methods: {
     login() {
       // 전체 유저에서 해당 이메일로 유저를 찾는다.
       let selectUser = null
       this.allUsers.forEach(user =>{
-        if(user.email === this.email) selectUser = user
+        if(user.name === this.email) selectUser = user
       })
       if (selectUser === null) alert('입력하신 이메일이 없습니다')
         else{
-        if (selectUser.password !== this.password){
+        if (selectUser.age !== this.password){
           alert('이메일과 비밀번호가 일치하지 않습니다.')
         }else {
           alert('로그인 완료')
@@ -62,6 +63,16 @@ export default {
       }
       // 그 유저의 비밀번호와 입력된 비밀번호를 비교한다.
       console.log(this.email, this.password)
+    },
+    getUsers () {
+      axios.get('http://localhost:3000/api/user')
+        .then((r) => {
+          this.allUsers = r.data.users
+          console.log(r)
+        })
+        .catch((e) => {
+          console.error(e.message)
+        })
     }
   }
 }
