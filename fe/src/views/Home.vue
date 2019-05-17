1bd2df81c473c0e02c36aced06fffd4284c3627b
dongguk kim <kdd0419@naver.com>
2019-05-17 10:33:38 +0900

    login dialog 추가

--- fe/src/views/Home.vue
+++ fe/src/views/Home.vue
@@ -2,7 +2,7 @@
   <v-container fluid>
     <v-slide-y-transition mode="out-in">
       <v-layout column align-center>
-        <v-btn color="info" href="/login">로그인</v-btn>
+        <v-btn color="info" @click="dialog = true">로그인</v-btn>
         <img src="@/assets/logo.png" alt="Vuetify.js" class="mb-5">
         <blockquote>
           &#8220;First, solve the problem. Then, write the code.&#8221;
@@ -12,10 +12,27 @@
             </small>
           </footer>
         </blockquote>
+        <v-dialog v-model="dialog">
+          <login ></login>
+        </v-dialog>
       </v-layout>
     </v-slide-y-transition>
   </v-container>
 </template>
+
+<script>
+import login from './login.vue'
+export default{
+  data(){
+    return {
+      dialog: false
+    }
+  },
+  components: {
+    'login': login
+  }
+}
+</script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
